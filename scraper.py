6ea13ad42f0bcac16a1c2dcf38659c37401adf27
Malcolm <mrussellclarke@gmail.com>
2018-11-30 13:48:52 -0500

    add check_mail, readmail functions

--- scraper.py
+++ scraper.py
@@ -1,18 +1,19 @@
-from scraperfunctions import find_flights
-from scraperfunctions import sendemail
+from scraperfunctions import find_flights, check_mail, sendemail
 # import collections
 # import multiprocessing
 
-lday = '1/23'
-rday = '2/1'
-here = 'LAX'
-elsw = 'DTW'
+flight_req = check_mail()
+
+lday = flight_req['outdate']
+here = flight_req['departure']
+elsw = flight_req['arrival']
+
+print('departure date: ' + lday)
+print('here: ' +here)
+print('there: ' +elsw)
 
 outbound_flights = find_flights(here, elsw, lday) #outgoing flight finder
-inbound_flights = find_flights(elsw, here, rday) #return flight finder
-
 lowest_out = outbound_flights[0].price
-lowest_in = inbound_flights[0].price
 
 subject      = 'Southwest Flights Between ' +here+ ' and ' +elsw
 message = ''
@@ -21,17 +22,22 @@ for f in outbound_flights:
     if lowest_out == f.price:
         message = message + f.name + ' on ' +lday+ ' is available for $' + f.price + '\n'
 
-for f in inbound_flights:
-    if lowest_in == f.price:
-        message = message + f.name + ' on ' + rday+ ' is available for $' + f.price + '\n'
+if 'indate' in flight_req:  #return flight finder
+    rday = flight_req['backdate']
+    inbound_flights = find_flights(elsw, here, rday)
+
+    lowest_in = inbound_flights[0].price
+
+    for f in inbound_flights:
+        if lowest_in == f.price:
+            message = message + f.name + ' on ' + rday+ ' is available for $' + f.price + '\n'
 
 print(subject)
 print(message)
 
-sendemail(subject, message)
+# sendemail(subject, message)
 
 #STILL TO DO
 #READ INCOMING EMAILS FOR FLIGHT CHECK REQUESTS
-# HOST ON HEROKU
 # RUN REPEATEDLY
 # STORE CHEAPEST FLIGHT AND UPDATE IF LOWEST PRICE CHANGES
--- scraperfunctions.py
+++ scraperfunctions.py
@@ -6,6 +6,10 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 import smtplib
+import time
+import imaplib
+import email
+import re
 
 class Flight:   #Flight Class used to store departure time, arrival time, and price data
     pass
@@ -53,9 +57,7 @@ def find_flights(start, end, date): #function to create list of flights leaving 
 
     return flight_class
 
-def sendemail(subject, message,
-              smtpserver='smtp.gmail.com:587'):
-
+def sendemail(subject, message, smtpserver='smtp.gmail.com:587'):
     header  = 'From: mcdevacct@gmail.com\n'
     header += 'To: mrussellclarke@gmail.com\n'
     # header += 'Cc: %s\n' % ','.join(cc_addr_list)
@@ -68,3 +70,80 @@ def sendemail(subject, message,
     problems = server.sendmail('mcdevacct@gmail.com', 'mrussellclarke@gmail.com', message)
     server.quit()
     return problems
+
+def check_mail():
+    email = readmail()
+    keywords = {}
+    email_body = []
+
+    # if 'mrussellclarke' in email['ef'] and 'flight' in email['es'].lower():
+    if 'mcdevacct' in email['ef']:
+        for line in email['eb'].splitlines():
+            email_body += line.split(' ')
+
+    for word in email_body:
+        if re.match(r'(\d|\d\d)/(\d\d|\d)', word, re.I):
+            if 'outdate' not in keywords:
+                keywords['outdate'] = word
+            else:
+                keywords['backdate'] = word
+        elif len(word) == 3:
+            # re.match(r'[A-z]{3}', word, re.I):
+            if 'departure' not in keywords:
+                keywords['departure'] = word
+            else:
+                keywords['arrival'] = word
+
+    return keywords
+
+def readmail():
+    mail = imaplib.IMAP4_SSL('imap.gmail.com')
+    mail.login('mcdevacct@gmail.com', 'S0uthw3stD3v')
+
+    mail.list()
+    mail.select('inbox')
+
+    result, data = mail.uid('search', None, "ALL")
+
+    latest_email_uid = data[0].split()[-1]
+    result, data= mail.uid('fetch', latest_email_uid, '(RFC822)')
+    raw_email = data[0][1]
+    raw_email_string = raw_email.decode('utf-8')
+
+    email_message = email.message_from_string(raw_email_string)
+
+    for part in email_message.walk():
+        if part.get_content_type() == "text/plain":
+            body = part.get_payload(decode=True)
+            email_from = email_message['from']
+            email_subject =  email_message['subject']
+            email_body = body.decode('utf-8')
+
+    # for part in email_message.walk():
+    #     if part.get_content_type() == "multipart/alternative":
+    #         print(part)
+    #
+    # i = len(data[0].split())
+    #
+    # for x in range(i):
+    #     latest_email_uid = data[0].split()[x]
+    #     result, email_data = mail.uid('fetch', latest_email_uid, '(RFC822)')
+    #     # fetch email body for given ID
+    #
+    #     raw_email = email_data[0][1]
+    #     raw_email_string = raw_email.decode('utf-8')
+    #
+    #     email_message = email.message_from_string(raw_email_string)
+    #
+    #     for part in email_message.walk():
+    #         if part.get_content_type() == "text/plain":
+    #             body = part.get_payload(decode=True)
+    #
+    #             print('From: ' +email_message['from'])
+    #             print('Subject: ' +email_message['subject'])
+    #             print(body)
+    #             # save_string = str("D:Dumpgmailemail_" + str(x) + ".eml")
+    #         else:
+    #             continue
+
+    return {'ef': email_from, 'es': email_subject, 'eb': email_body}
